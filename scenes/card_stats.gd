c3cbc77f934e1cb85ca9b003f67bc5480ca3f9e9
RealHumanPerson3141 <103381058+RealHumanPerson3141@users.noreply.github.com>
2025-12-22 19:19:36 +0200

    style: reorder variables

--- scenes/card_stats.gd
+++ scenes/card_stats.gd
@@ -8,10 +8,11 @@ extends Resource
 @export_group("Speeds")
 @export var move_speed: int
 @export var hit_speed: float
+@export var first_hit_speed: float
 
 @export_group("Deploy")
-@export var first_hit_speed: float
-@export var deploy_time: float
+@export var count: int = 1
+@export var deploy_time: float = 1
 
 @export_group("Attack Range")
 @export var sight_range: float
